7308b63096a52e1066243fc509c39bd11416ead2
Aimin Guo <amguo@live.com>
2022-09-27 12:20:58 -0700

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,6 @@
-import play.api.libs.json._
 
-case class Car(make: String, model: String, cost: Int, madeTS: Option[Long] = None)
-object Car {
-  implicit val carReads: Reads[Car] = Json.reads[Car]
-}
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-
-    val json =
-      """
-        |[
-        |    {
-        |       "make": "Tesla",
-        |       "model": "Model S",
-        |       "cost": 50000
-        |    },
-        |    {
-        |       "make": "Tesla",
-        |       "model": "Model D",
-        |       "cost": 30000
-        |    }
-        |]
-  """.stripMargin
-
-    val parsedJsValue = Json.parse(json)
-    val parsed  = Json.fromJson[List[Car]](parsedJsValue)
-    println(parsed)
   }
 }
